bf11ec0894057765de48b67c520b04af066d202d
Annalise Tarhan <annalisetarhan@gmail.com>
2020-10-24 20:28:15 +0300

    Export to CSV

---
+++ ratScraper.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Set read_from_file to true to parse a
+# cached page instead of fetching a new copy.
+read_from_file = True
+
+if read_from_file:
+    page = open("cached_page.html", "r")
+    soup = BeautifulSoup(page, 'html.parser')
+else:
+    page = requests.get("https://www.remote-associates-test.com")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    # Save a copy of the page locally
+    saved_page = open("cached_page.html", "w")
+    saved_page.write(page.text)
+    saved_page.close()
+
+results = open("results.csv", "w")
+words = ''
+answer = ''
+difficulty = ''
+error_count = 0
+
+for tr in soup.find_all('tr'):
+    try:
+        words = tr.find('a').contents[0]
+    except:
+        error_count += 1
+        continue
+    try:
+        answer = tr.find_all('span')[0].contents[0]
+    except:
+        error_count += 1
+        continue
+    try:
+        difficulty = tr.find_all('span')[1].contents[0]
+    except:
+        # Some questions don't have difficulties. Assume medium.
+        difficulty = "Medium"
+        
+    results.write("{}, {}, {}\n".format(words, answer, difficulty))
+    
+results.close()
+
+# Page's table header looks like a question, so it looks like an error,
+# so expected value of error_count is 1.
+if error_count > 1:
+    print("Error: {} questions not saved".format(error_count))
